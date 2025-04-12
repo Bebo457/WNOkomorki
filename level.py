64cf3a305dbc0dee9ca25ae51fecadaa961eb23b
Bebo457 <bartekbab845@gmail.com>
2025-04-12 08:57:17 +0200

    commit

--- level.py
+++ level.py
@@ -1192,6 +1192,14 @@ class Level:
                 self.pvp_main_menu.disable()
                 shared.timer_menu.disable()
 
+    # wywoływane po inicjalizowaniu połączenia po stronie klienta
+    def initialize_online_game(self):
+        if not shared.is_host:
+            shared.game_state = 'online_setup'
+            self.pvp_side_menu_setup()
+            shared.timer_menu.disable()
+            shared.pvp_menu.disable()
+
 
 def draw_dashed_line(screen, color, start_pos, end_pos, dash_length=12, width=line_width):
     x1, y1 = start_pos
--- online.py
+++ online.py
@@ -40,8 +40,8 @@ def connect_to_game():
                 print("Połączenie zostało zaakceptowane przez hosta")
                 # ------------------------------------TUTAJ DAWAĆ RZECZY PRZY INICJALICAJI KLIENTA ---------------------
                 shared.active_connection = True
-                shared.game_state = 'online_setup'
-                shared.level.pvp_side_menu_setup()
+                shared.level.initialize_online_game()
+
                 if hasattr(shared, 'online_status_label'):
                     shared.online_status_label.set_title("połączono")
             else:
