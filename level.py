af79cdc1d2cfc0d3e77679d7c9625cba3dbaf118
Bebo457 <bartekbab845@gmail.com>
2025-04-12 08:15:09 +0200

    commit

--- level.py
+++ level.py
@@ -1175,6 +1175,7 @@ class Level:
             shared.pvp_menu.disable()
 
     def start_new_turn_online(self):
+        pass
 
 
 def draw_dashed_line(screen, color, start_pos, end_pos, dash_length=12, width=line_width):
